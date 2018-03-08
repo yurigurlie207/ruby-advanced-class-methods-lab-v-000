2e717183ab19d55f356a9f41fd323ffa3fa3626a
Adrienne Yao <adrienne.yao@icloud.com>
2018-03-08 02:08:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,7 @@ class Song
     }
   end
 
-  def self.find_or_create_by_name(name)
+  def self.find_or_create_by_name(name,artist_name)
 
     if self.find_by_name(name)
       self.find_by_name(name)
