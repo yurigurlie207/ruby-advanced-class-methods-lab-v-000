5a58c0abff7a331bdb712fc3db3c3b34e4ed204b
Adrienne Yao <adrienne.yao@icloud.com>
2018-03-08 01:50:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,7 +55,7 @@ class Song
   end
 
   def self.new_from_filename(name)
-    songName = name.split(" - ")[1].split(".")[0]
+    songName = name.split("-")[1].split(".")[0]
     self.create_by_name(songName)
   end
 
